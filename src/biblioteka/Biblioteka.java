c4803d2eaf325e54ef523340777b87b6a2ed49dc
nevena-djuricic <dj.nevena@hotmail.com>
2015-03-19 21:07:32 +0100

    Dodate if naredbe kao logisticka podrska klasi Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -9,16 +9,31 @@ public class Biblioteka implements BibliotekaInterfejs {
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	
 	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new RuntimeException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga vec postoji");
+		
 		knjige.add(knjiga);
 	}
 
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new RuntimeException("Knjiga ne sme biti null");
+		
+		if (!knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne postoji");
+		
 		knjige.remove(knjiga);
 	}
 
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, long ISBN,
 			String izdavac, Autor autor) {
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		if (naslov == null && ISBN <= 0 && izdavac == null && autor == null)
+			return knjige;
 		
 		if (ISBN > 0) {
 			for (int i = 0; i < knjige.size(); i++) {
